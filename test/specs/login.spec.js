ae59b856b7205c6cad9740efc1271d0ebf4e2e4b
Takeshi Kishi <takeya0x86@gmail.com>
2021-11-21 18:03:11 +0000

    make async mode

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -3,35 +3,35 @@ const MyPage = require('../pageobjects/my.page');
 const TopPage = require('../pageobjects/top.page');
 
 describe('Login', () => {
-  it('should be successful logged in preset user', () => {
-    TopPage.open();
-    TopPage.goToLoginPage();
-    LoginPage.email.setValue('clark@example.com');
-    LoginPage.password.setValue('password');
-    LoginPage.submit();
+  it('should be successful logged in preset user', async () => {
+    await TopPage.open();
+    await TopPage.goToLoginPage();
+    await LoginPage.email.setValue('clark@example.com');
+    await LoginPage.password.setValue('password');
+    await LoginPage.submit();
 
-    expect(MyPage.header).toHaveText('MyPage');
+    await expect(MyPage.header).toHaveText('MyPage');
   });
 
-  it('should be an error when empty input', () => {
-    TopPage.open();
-    TopPage.goToLoginPage();
-    LoginPage.email.setValue('');
-    LoginPage.password.setValue('');
-    LoginPage.submit();
+  it('should be an error when empty input', async () => {
+    await TopPage.open();
+    await TopPage.goToLoginPage();
+    await LoginPage.email.setValue('');
+    await LoginPage.password.setValue('');
+    await LoginPage.submit();
 
-    expect(LoginPage.emailMessage).toHaveText('Please fill out this field.');
-    expect(LoginPage.passwordMessage).toHaveText('Please fill out this field.');
+    await expect(LoginPage.emailMessage).toHaveText('Please fill out this field.');
+    await expect(LoginPage.passwordMessage).toHaveText('Please fill out this field.');
   });
 
-  it('should be an error when invalid user', () => {
-    TopPage.open();
-    TopPage.goToLoginPage();
-    LoginPage.email.setValue('error@example.com');
-    LoginPage.password.setValue('error');
-    LoginPage.submit();
+  it('should be an error when invalid user', async () => {
+    await TopPage.open();
+    await TopPage.goToLoginPage();
+    await LoginPage.email.setValue('error@example.com');
+    await LoginPage.password.setValue('error');
+    await LoginPage.submit();
 
-    expect(LoginPage.emailMessage).toHaveText('Email or password is invalid.');
-    expect(LoginPage.passwordMessage).toHaveText('Email or password is invalid.');
+    await expect(LoginPage.emailMessage).toHaveText('Email or password is invalid.');
+    await expect(LoginPage.passwordMessage).toHaveText('Email or password is invalid.');
   });
 });
--- test/specs/plans.spec.js
+++ test/specs/plans.spec.js
@@ -3,62 +3,62 @@ const MyPage = require('../pageobjects/my.page');
 const PlansPage = require('../pageobjects/plans.page');
 const TopPage = require('../pageobjects/top.page');
 
-describe('Plans', () => {
-  it('should be display plans when not logged in', () =>{
-    TopPage.open();
-    TopPage.goToPlansPage();
-    const planTitles = PlansPage.getPlanTitles();
+describe('Plans', async () => {
+  it('should be display plans when not logged in', async () => {
+    await TopPage.open();
+    await TopPage.goToPlansPage();
+    const planTitles = await PlansPage.getPlanTitles();
 
-    expect(planTitles).toHaveLength(7);
-    expect(planTitles[0]).toHaveText('Plan with special offers');
-    expect(planTitles[1]).toHaveText('Staying without meals');
-    expect(planTitles[2]).toHaveText('Business trip');
-    expect(planTitles[3]).toHaveText('With beauty salon');
-    expect(planTitles[4]).toHaveText('With private onsen');
-    expect(planTitles[5]).toHaveText('For honeymoon');
-    expect(planTitles[6]).toHaveText('With complimentary ticket');
+    await expect(planTitles).toHaveLength(7);
+    await expect(planTitles[0]).toHaveText('Plan with special offers');
+    await expect(planTitles[1]).toHaveText('Staying without meals');
+    await expect(planTitles[2]).toHaveText('Business trip');
+    await expect(planTitles[3]).toHaveText('With beauty salon');
+    await expect(planTitles[4]).toHaveText('With private onsen');
+    await expect(planTitles[5]).toHaveText('For honeymoon');
+    await expect(planTitles[6]).toHaveText('With complimentary ticket');
   });
 
-  it('should be display plans when logged in member', () =>{
-    TopPage.open();
-    TopPage.goToLoginPage();
-    LoginPage.email.setValue('diana@example.com');
-    LoginPage.password.setValue('pass1234');
-    LoginPage.submit();
-    MyPage.goToPlansPage();
-    const planTitles = PlansPage.getPlanTitles();
+  it('should be display plans when logged in member', async () => {
+    await TopPage.open();
+    await TopPage.goToLoginPage();
+    await LoginPage.email.setValue('diana@example.com');
+    await LoginPage.password.setValue('pass1234');
+    await LoginPage.submit();
+    await MyPage.goToPlansPage();
+    const planTitles = await PlansPage.getPlanTitles();
 
-    expect(planTitles).toHaveLength(9);
-    expect(planTitles[0]).toHaveText('Plan with special offers');
-    expect(planTitles[1]).toHaveText('With dinner');
-    expect(planTitles[2]).toHaveText('Economical');
-    expect(planTitles[3]).toHaveText('Staying without meals');
-    expect(planTitles[4]).toHaveText('Business trip');
-    expect(planTitles[5]).toHaveText('With beauty salon');
-    expect(planTitles[6]).toHaveText('With private onsen');
-    expect(planTitles[7]).toHaveText('For honeymoon');
-    expect(planTitles[8]).toHaveText('With complimentary ticket');
+    await expect(planTitles).toHaveLength(9);
+    await expect(planTitles[0]).toHaveText('Plan with special offers');
+    await expect(planTitles[1]).toHaveText('With dinner');
+    await expect(planTitles[2]).toHaveText('Economical');
+    await expect(planTitles[3]).toHaveText('Staying without meals');
+    await expect(planTitles[4]).toHaveText('Business trip');
+    await expect(planTitles[5]).toHaveText('With beauty salon');
+    await expect(planTitles[6]).toHaveText('With private onsen');
+    await expect(planTitles[7]).toHaveText('For honeymoon');
+    await expect(planTitles[8]).toHaveText('With complimentary ticket');
   });
 
-  it('should be display plans when logged in premium member', () =>{
-    TopPage.open();
-    TopPage.goToLoginPage();
-    LoginPage.email.setValue('clark@example.com');
-    LoginPage.password.setValue('password');
-    LoginPage.submit();
-    MyPage.goToPlansPage();
-    const planTitles = PlansPage.getPlanTitles();
+  it('should be display plans when logged in premium member', async () => {
+    await TopPage.open();
+    await TopPage.goToLoginPage();
+    await LoginPage.email.setValue('clark@example.com');
+    await LoginPage.password.setValue('password');
+    await LoginPage.submit();
+    await MyPage.goToPlansPage();
+    const planTitles = await PlansPage.getPlanTitles();
 
-    expect(planTitles).toHaveLength(10);
-    expect(planTitles[0]).toHaveText('Plan with special offers');
-    expect(planTitles[1]).toHaveText('Premium plan');
-    expect(planTitles[2]).toHaveText('With dinner');
-    expect(planTitles[3]).toHaveText('Economical');
-    expect(planTitles[4]).toHaveText('Staying without meals');
-    expect(planTitles[5]).toHaveText('Business trip');
-    expect(planTitles[6]).toHaveText('With beauty salon');
-    expect(planTitles[7]).toHaveText('With private onsen');
-    expect(planTitles[8]).toHaveText('For honeymoon');
-    expect(planTitles[9]).toHaveText('With complimentary ticket');
+    await expect(planTitles).toHaveLength(10);
+    await expect(planTitles[0]).toHaveText('Plan with special offers');
+    await expect(planTitles[1]).toHaveText('Premium plan');
+    await expect(planTitles[2]).toHaveText('With dinner');
+    await expect(planTitles[3]).toHaveText('Economical');
+    await expect(planTitles[4]).toHaveText('Staying without meals');
+    await expect(planTitles[5]).toHaveText('Business trip');
+    await expect(planTitles[6]).toHaveText('With beauty salon');
+    await expect(planTitles[7]).toHaveText('With private onsen');
+    await expect(planTitles[8]).toHaveText('For honeymoon');
+    await expect(planTitles[9]).toHaveText('With complimentary ticket');
   });
 });
